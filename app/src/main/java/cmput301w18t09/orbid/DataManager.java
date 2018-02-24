b0065ff5437bf95400b66f887856b0daf9c856ca
dlaycock <dlaycock@ualberta.ca>
2018-02-23 22:45:04 -0700

    Created DataManager Class

---
+++ app/src/main/java/cmput301w18t09/orbid/DataManager.java
@@ -0,0 +1,22 @@
+package cmput301w18t09.orbid;
+
+/**
+ * Created by david on 23/02/18.
+ */
+
+public class DataManager {
+
+    private final String url = "http://cmput301.softwareprocess.es:8080/CMPUT301W18T09/";
+
+    public DataManager() {}
+
+    public void addObjects(Object T) {}
+
+    public Object getObjects() {}
+
+    public void updateObject(Object T) {}
+
+    public Boolean userExists(String username) {}
+
+    private Boolean isServerConnectable() {}
+}
