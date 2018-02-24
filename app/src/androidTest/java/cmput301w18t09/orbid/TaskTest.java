9db7f24ed2df39e09d6c88e7a20938af41253c03
Chady <chady@ualberta.ca>
2018-02-24 15:51:37 -0700

    Added enum for Task class and added(mostly finished) Unit test for Task class.

---
+++ app/src/androidTest/java/cmput301w18t09/orbid/TaskTest.java
@@ -0,0 +1,61 @@
+package cmput301w18t09.orbid;
+
+import android.media.Image;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by Chady on 2018-02-15.
+ */
+
+public class TaskTest {
+
+    public TaskTest() {
+
+    }
+
+    public void testAddBid() {
+        User testUser = new User("Test", "Test", "Test", "Test", "test");
+        Bid testBid = new Bid(testUser, 3.14, "test" );
+        Task testTask = new Task(testUser, "test", "test", 3.1, 5);
+        testTask.addBid(testBid);
+        assertTrue(testTask.containsBid(testBid));
+    }
+
+    public void testRepost() {
+        User testUser = new User("Test", "Test", "Test", "Test", "test");
+        Bid testBid = new Bid(testUser, 3.14, "test" );
+        Task testTask = new Task(testUser, "test", "test", 3.1, 4);
+        assertTrue(testTask.getTaskStatus() == TaskStatus.COMPLETED);
+        testTask.repost();
+        assertTrue(testTask.getTaskStatus() == TaskStatus.REQUESTED);
+    }
+
+    public void testAddPhoto() {
+        User testUser = new User("Test", "Test", "Test", "Test", "test");
+        Bid testBid = new Bid(testUser, 3.14, "test" );
+        Task testTask = new Task(testUser, "test", "test", 3.1, 5);
+        Image testImage = ;
+        testTask.addPhoto(testImage);
+        assertTrue(testTask.containsPhoto(testImage));
+    }
+
+    public void testAcceptBid() {
+        User testUser = new User("Test", "Test", "Test", "Test", "test");
+        Bid testBid = new Bid(testUser, 3.14, "test" );
+        Task testTask = new Task(testUser, "test", "test", 3.1, 5);
+        testTask.addBid(testBid);
+        testTask.acceptBid(0);
+        assertTrue(testTask.getAcceptedBid() == testBid);
+    }
+
+    public void testDeclineBid() {
+        User testUser = new User("Test", "Test", "Test", "Test", "test");
+        Bid testBid = new Bid(testUser, 3.14, "test" );
+        Task testTask = new Task(testUser, "test", "test", 3.1, 5);
+        testTask.addBid(testBid);
+        testTask.declineBid(0);
+        assertTrue(testTask.containsBid(testBid));
+    }
+}
