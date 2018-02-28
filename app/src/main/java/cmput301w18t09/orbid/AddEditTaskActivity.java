69b545245495a1088b1d52db49e45eca2f2db1ab
Chady <chady@ualberta.ca>
2018-02-27 17:26:51 -0700

    Changed the navigation bar, had to redo it in AddEditTask

--- app/src/main/java/cmput301w18t09/orbid/AddEditTaskActivity.java
+++ app/src/main/java/cmput301w18t09/orbid/AddEditTaskActivity.java
@@ -1,6 +1,13 @@
 package cmput301w18t09.orbid;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,11 +23,18 @@ public class AddEditTaskActivity extends NavigationActivity {
     private EditText etLocation;
     private EditText etPrice;
     private ArrayList<Bid> bidList = new ArrayList<Bid>();
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_task);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
         User testUser = new User("test", "test","test", "test", "test");
         Bid testBid = new Bid(testUser, 3.14, "test");
         Bid testBid2 = new Bid(testUser, 3.14, "test");
@@ -31,6 +45,16 @@ public class AddEditTaskActivity extends NavigationActivity {
         ListView listView = (ListView) findViewById(R.id.BidList);
         BidListAdapter bidAdapter = new BidListAdapter(this, bidList);
         listView.setAdapter(bidAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void save()
