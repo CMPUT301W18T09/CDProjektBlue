9555076fe4efb7c9ac4f68035a5a8362a585def6
AidanKosik <aidankosik@mac.com>
2018-03-01 13:36:50 -0700

    import button in nav now opens recent listings

--- app/src/main/java/cmput301w18t09/orbid/RecentListingsActivity.java
+++ app/src/main/java/cmput301w18t09/orbid/RecentListingsActivity.java
@@ -1,10 +1,19 @@
 package cmput301w18t09.orbid;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.ToggleButton;
-import android.widget.Toolbar;
+
 
 import java.util.ArrayList;
 
@@ -14,20 +23,45 @@ public class RecentListingsActivity extends NavigationActivity {
     private ListView listView;
     private TaskListAdapter taskListAdapter;
     private ToggleButton tbtnToggle;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         // Must call in this order, NOT super first
-        setContentView(R.layout.activity_recent_listings);
         super.onCreate(savedInstanceState);
+        int layoutID = getIntent().getIntExtra("recent_listings_layout_id", 0);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        FrameLayout frameLayout = findViewById(R.id.navigation_content_frame);
+        inflater.inflate(layoutID, frameLayout);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         Switch change_view_switch = new Switch(getBaseContext());
-        change_view_switch.setTextOn("List View");
-        change_view_switch.setTextOff("Map View");
+        change_view_switch.setTextOn("List");
+        change_view_switch.setTextOff("Map");
         change_view_switch.setChecked(true);
+//        change_view_switch.setOnCheckedChangeListener(new OnCheckedChangedListener(
+//
+//        );
         toolbar.addView(change_view_switch);
 
+
+    }
+
+    @Override
+    public void onCheckChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     // Will need to parse string for individual keywords
@@ -51,5 +85,7 @@ public class RecentListingsActivity extends NavigationActivity {
     {
 
     }
+
+
 
 }
