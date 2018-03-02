eb16af2e324f0e132ea06d65595805a3481f6285
AidanKosik <aidankosik@mac.com>
2018-03-01 19:29:00 -0700

    Started the card view holder and adapter

--- app/src/main/java/cmput301w18t09/orbid/TaskListAdapter.java
+++ app/src/main/java/cmput301w18t09/orbid/TaskListAdapter.java
@@ -1,44 +1,45 @@
 package cmput301w18t09.orbid;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 
 
-public class TaskListAdapter extends ArrayAdapter<Task> {
+public class TaskListAdapter extends RecyclerView.Adapter<TaskViewHolder> {
 
     private Context context;
     private ArrayList<Task> taskList;
 
-    public TaskListAdapter(Context context, ArrayList<Task> taskList)
-    {
-        super(context, 0, taskList);
+    public TaskListAdapter(final Context context, ArrayList<Task> taskList) {
         this.context = context;
         this.taskList = taskList;
-
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        View taskItem = convertView;
-
-        // A short example of inflating a bid item in the list
-
-        // if(taskItem == null) {
-        //    bidItem = LayoutInflater.from(context).inflate(R.layout.bid_item, parent, false);
-        // }
-
-        // Task task = taskList.get(position);
-        // TextView tvDescription = (TextView) taskItem.findViewById(R.id.task_list_layout);
-        // tvDescription.setText(task.getDescription());
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        View view = inflater.inflate(R.layout.activity_recent_listings, parent, false);
+        return new TaskViewHolder(view, this.context);
+    }
 
 
-        return taskItem;
+    @Override
+    public void onBindViewHolder(TaskViewHolder holder, int position) {
+        Task task = taskList.get(position);
+        holder.task_image.
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
     }
 
 }
