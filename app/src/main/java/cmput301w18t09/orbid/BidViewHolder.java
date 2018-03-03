c959664a3396e0ca8dd3ddc4b0dbdd9cb6cca7a4
AidanKosik <aidankosik@mac.com>
2018-03-03 15:42:14 -0700

    started bidViewHolder and BidViewAdapter

---
+++ app/src/main/java/cmput301w18t09/orbid/BidViewHolder.java
@@ -0,0 +1,29 @@
+package cmput301w18t09.orbid;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by aidankosik on 2018-03-03.
+ */
+
+public class BidViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView bid_price;
+    public TextView bid_description;
+
+    public BidViewHolder(View view, final Context context) {
+        super(view);
+
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Timber", "NICE CLICK BRO");
+            }
+        });
+    }
+}
