040a9a8b3be358ead9c00321e18fcbfc482026ab
AidanKosik <aidankosik@mac.com>
2018-03-03 15:42:14 -0700

    started bidViewHolder and BidViewAdapter

--- app/src/main/java/cmput301w18t09/orbid/BidListAdapter.java
+++ app/src/main/java/cmput301w18t09/orbid/BidListAdapter.java
@@ -1,6 +1,7 @@
 package cmput301w18t09.orbid;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,36 +11,35 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 
-public class BidListAdapter extends ArrayAdapter {
+public class BidListAdapter extends RecyclerView.Adapter<BidViewHolder> {
 
     private Context context;
     private ArrayList<Bid> bidList;
 
-    public BidListAdapter(Context context, ArrayList<Bid> bidList)
-    {
-        super(context, 0, bidList);
+    public BidListAdapter(final Context context, ArrayList<Bid> bidList) {
         this.context = context;
         this.bidList = bidList;
-
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        View bidItem = convertView;
-
-        // An short example of inflating a bid item in the list
-
-        if(bidItem == null) {
-            bidItem = LayoutInflater.from(context).inflate(R.layout.bid_listview_layout, parent, false);
-         }
-
-        Bid bid = bidList.get(position);
-        TextView bidPrice = (TextView) bidItem.findViewById(R.id.BidPrice);
-        TextView bidComment = (TextView) bidItem.findViewById(R.id.BidComment);
-
-        bidPrice.setText(Double.toString(bid.getPrice()));
-        bidComment.setText(bid.getDescription());
-        return bidItem;
+    public BidViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        View view = inflater.inflate(R.layout.activity_recent_listings, parent, false);
+        return new BidViewHolder(view, this.context);
     }
+
+
+    @Override
+    public void onBindViewHolder(BidViewHolder holder, int position) {
+        Bid bid = bidList.get(position);
+
+        // Fill out bid information
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return bidList.size();
+    }
+
 }
