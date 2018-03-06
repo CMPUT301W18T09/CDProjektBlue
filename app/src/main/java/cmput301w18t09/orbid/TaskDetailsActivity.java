b76fb9654cf8fb24c1753861f3488a76f6ffa7fa
AidanKosik <aidankosik@mac.com>
2018-03-05 19:12:42 -0700

    added map functinoality, task details, and onClick for recentListings

--- app/src/main/java/cmput301w18t09/orbid/TaskDetailsActivity.java
+++ app/src/main/java/cmput301w18t09/orbid/TaskDetailsActivity.java
@@ -1,13 +1,57 @@
 package cmput301w18t09.orbid;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TaskDetailsActivity extends NavigationActivity{
 
+    private DrawerLayout mDrawerLayout;
+    private ArrayList<Task> taskList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_task_details);
         super.onCreate(savedInstanceState);
+
+        int layoutID = getIntent().getIntExtra("task_details_layout_id", 0);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        FrameLayout frameLayout = findViewById(R.id.navigation_content_frame);
+        inflater.inflate(layoutID, frameLayout);
+
+        TextView task_title = findViewById(R.id.task_title);
+        TextView task_description = findViewById(R.id.task_description);
+//        TextView task_lowest_bid = findViewById(R.id.lowest_bid);
+
+        taskList = new ArrayList<Task>();
+        User user = new User("NAN", "nan@gmail.com", "1", "NAN", "THE MAN");
+        Task task = new Task(user, "SOME TASK", "TESTING TASK", 10, Task.TaskStatus.BIDDED);
+        taskList.add(task);
+
+        // Todo get task from data manager
+
+        task_title.setText(task.getTitle());
+        task_description.setText(task.getDescription());
+        // Todo set lowest bid
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
