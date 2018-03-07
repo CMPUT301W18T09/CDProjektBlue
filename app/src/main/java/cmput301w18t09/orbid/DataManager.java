bfca06a7682d8b4328ab3320405862531dffeb5c
Ceegan Hale <ceeganhale@gmail.com>
2018-03-06 22:48:01 -0700

    Adds offline behaviour

--- app/src/main/java/cmput301w18t09/orbid/DataManager.java
+++ app/src/main/java/cmput301w18t09/orbid/DataManager.java
@@ -1,8 +1,11 @@
 package cmput301w18t09.orbid;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
@@ -10,6 +13,14 @@ import com.searchly.jestdroid.JestDroidClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +36,19 @@ import io.searchbox.core.SearchResult;
 
 public class DataManager {
     private static JestDroidClient client;
-    private ArrayList<Task> cachedTasks;
-    private ArrayList<User> cachedUsers;
-
+    private static ArrayList<Task> cachedTasks;
+    private static final String tasksFile = "taskList.sav";
+    private static Context context;
     /**
      * Inserts new tasks into the database
-     *
      */
     public static class addTasks extends AsyncTask<Task, Void, Void> {
+
+        public addTasks(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see addTasks
@@ -45,22 +61,25 @@ public class DataManager {
 
             for (Task task: tasks){
                 Index index = new Index.Builder(task).index("cmput301w18t09").type("task").build();
-
                 try{
                     DocumentResult result = client.execute(index);
                     if (result.isSucceeded()){
                         task.setID(result.getId());
                         Log.v("id", task.getID());
+                        pushCached();
                     }
                     else{
-                        //TODO: Offline behaviour
+                        cachedTasks.add(task);
+                        saveInFile();
                     }
                 }
                 catch (Exception e){
                     Log.e("Error", "The application has failed to build and send the task");
-                    //TODO: Offline behaviour
+                    cachedTasks.add(task);
+                    saveInFile();
                 }
             }
+
             return null;
         }
     }
@@ -69,6 +88,12 @@ public class DataManager {
      * Inserts new Users into the database
      */
     public static class addUsers extends AsyncTask<User, Void, Void> {
+
+        public addUsers(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see addUsers
@@ -80,7 +105,7 @@ public class DataManager {
             verifySettings();
 
             for (User user: users){
-                Index index = new Index.Builder(user).index("cmput301w18t09").type("task").build();
+                Index index = new Index.Builder(user).index("cmput301w18t09").type("user").build();
 
                 try{
                     DocumentResult result = client.execute(index);
@@ -88,13 +113,9 @@ public class DataManager {
                         user.setID(result.getId());
                         Log.v("id", user.getID());
                     }
-                    else{
-                        //TODO: Offline behaviour
-                    }
                 }
                 catch (Exception e){
                     Log.e("Error", "The application has failed to build and send the task");
-                    //TODO: Offline behaviour
                 }
             }
             return null;
@@ -105,6 +126,12 @@ public class DataManager {
      * gets a list of tasks based on the search parameters
      */
     public static class getTasks extends AsyncTask<ArrayList<String>, Void, ArrayList<Task>>{
+
+        public getTasks(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see getTasks
@@ -129,16 +156,15 @@ public class DataManager {
                 if (result.isSucceeded()){
                     List<Task> foundTasks = result.getSourceAsObjectList(Task.class);
                     tasks.addAll(foundTasks);
+                    Log.v("return", result.getJsonString());
+                    pushCached();
                 }
                 else {
                     Log.e("Error", "Search query failed");
-                    //TODO: Offline behaviour
                 }
             }
             catch (Exception e){
                 Log.e("Error", "Failed to communicate to elastic search server");
-                e.printStackTrace();
-                //TODO: Offline behaviour
             }
 
             return tasks;
@@ -149,6 +175,12 @@ public class DataManager {
      * gets a list of tasks based on the search parameters
      */
     public static class getUsers extends AsyncTask<ArrayList<String>, Void, ArrayList<User>>{
+
+        public getUsers(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see getUsers
@@ -176,13 +208,11 @@ public class DataManager {
                 }
                 else {
                     Log.e("Error", "Search query failed");
-                    //TODO: Offline behaviour
                 }
             }
             catch (Exception e){
                 Log.e("Error", "Failed to communicate to elastic search server");
                 e.printStackTrace();
-                //TODO: Offline behaviour
             }
 
             return users;
@@ -193,6 +223,12 @@ public class DataManager {
      * updates tasks in the database
      */
     public static class updateTasks extends AsyncTask<ArrayList<Task>, Void, Void>{
+
+        public updateTasks(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see updateTasks
@@ -210,16 +246,19 @@ public class DataManager {
 
                     if (result.isSucceeded()){
                         Log.v("Success", "Task has been updated");
+                        pushCached();
                     }
                     else{
                         Log.e("Error", "Update has failed");
-                        //TODO: Offline behaviour
+                        cachedTasks.add(task);
+                        saveInFile();
                     }
 
                 }
                 catch (Exception e){
                     Log.e("Error", "Failed to connect to the elastic search server");
-                    //TODO: Offline behaviour
+                    cachedTasks.add(task);
+                    saveInFile();
                 }
             }
 
@@ -231,6 +270,12 @@ public class DataManager {
      * updates users in the database
      */
     public static class updateUsers extends AsyncTask<ArrayList<User>, Void, Void>{
+
+        public updateUsers(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see updateUsers
@@ -250,14 +295,12 @@ public class DataManager {
                         Log.v("Success", "User has been updated");
                     }
                     else{
-                        Log.e("Error", "Update has failed");
-                        //TODO: Offline behavior
+                        Log.e("Error", "User update has failed");
                     }
 
                 }
                 catch (Exception e){
                     Log.e("Error", "Failed to connect to the elastic search server");
-                    //TODO: Offline behaviour
                 }
             }
 
@@ -269,6 +312,12 @@ public class DataManager {
      * Deletes tasks from the database
      */
     public static class deleteTasks extends AsyncTask<ArrayList<String>, Void, Void>{
+
+        public deleteTasks(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
 
         /**
          * @see deleteTasks
@@ -286,16 +335,15 @@ public class DataManager {
 
                     if (result.isSucceeded()){
                         Log.v("Success", "Task has been Deleted");
+                        pushCached();
                     }
                     else{
-                        Log.e("Error", "Delete has failed");
-                        //TODO: Offline behaviour
+                        Log.e("Error", "Task delete has failed");
                     }
 
                 }
                 catch (Exception e){
                     Log.e("Error", "Failed to connect to the elastic search server");
-                    //TODO: Offline behaviour
                 }
             }
 
@@ -308,6 +356,11 @@ public class DataManager {
      */
     public static class deleteUsers extends AsyncTask<ArrayList<String>, Void, Void>{
 
+        public deleteUsers(Context cont){
+            if (context == null){
+                context = cont;
+            }
+        }
         /**
          * @see deleteUsers
          * @param passed An array list of user IDs
@@ -327,13 +380,11 @@ public class DataManager {
                     }
                     else{
                         Log.e("Error", "Delete has failed");
-                        //TODO: Offline behavior
                     }
 
                 }
                 catch (Exception e){
                     Log.e("Error", "Failed to connect to the elastic search server");
-                    //TODO: Offline behaviour
                 }
             }
 
@@ -344,7 +395,7 @@ public class DataManager {
     /**
      * Verifies that the client has been established before trying to call server
      */
-    public static void verifySettings(){
+    private static void verifySettings(){
         if (client == null){
             DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
             DroidClientConfig config = builder.build();
@@ -352,6 +403,80 @@ public class DataManager {
             JestClientFactory factory = new JestClientFactory();
             factory.setDroidClientConfig(config);
             client = (JestDroidClient) factory.getObject();
+
+            cachedTasks = new ArrayList<>();
+        }
+
+        if (!cachedTasks.isEmpty()){
+            loadFromFile();
         }
     }
+
+    /**
+     * Loads cached tasks that need to be synced to the server
+     */
+    private static void loadFromFile(){
+        try{
+            Gson gson = new Gson();
+            FileInputStream fis = context.openFileInput(tasksFile);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            cachedTasks = gson.fromJson(in, new TypeToken<ArrayList<Task>>(){}.getType());
+            Log.i("Offline", "Cached tasks have been loaded");
+        }catch (FileNotFoundException e){
+            Log.e("Error", "Cached tasks could not be loaded");
+            throw new RuntimeException();
+        }
+
+    }
+
+    /**
+     * Pushes all cached tasks to server once a successful server call has been made
+     */
+    private static void pushCached(){
+        for (Task task: cachedTasks){
+            Index index;
+            if (task.getID().length() == 0){
+                index = new Index.Builder(task).index("cmput301w18t09").type("task").build();
+            }
+            else{
+                index = new Index.Builder(task).index("cmput301w18t09").type("user").id(task.getID()).build();
+            }
+
+
+            try{
+                DocumentResult result = client.execute(index);
+                if (result.isSucceeded()){
+                    if (task.getID().length() == 0) {
+                        task.setID(result.getId());
+                        Log.v("id", task.getID());
+                    }
+                    cachedTasks.remove(task);
+                }
+            }
+            catch (Exception e){
+                Log.e("Error", "The application has failed to communicate with the server");
+            }
+        }
+        saveInFile();
+        Log.i("Offline", "cached tasks have been pushed to server");
+    }
+
+    /**
+     * Saves the cached tasks to a file for better offline behaviour
+     */
+    private static void saveInFile(){
+
+        try{
+            FileOutputStream fos = context.openFileOutput(tasksFile, Context.MODE_PRIVATE);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+            Gson gson = new Gson();
+            gson.toJson(cachedTasks, writer);
+            writer.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Log.i("Offline", "cached tasks have been stored");
+    }
+
+
 }
