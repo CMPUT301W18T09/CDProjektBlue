f633ef879d62d08e5bd4b4bfce7a9b34b45ec6af
dlaycock <dlaycock@ualberta.ca>
2018-03-08 22:16:02 -0700

    Restored DataManagerTest

--- app/src/androidTest/java/cmput301w18t09/orbid/DataManagerTest.java
+++ app/src/androidTest/java/cmput301w18t09/orbid/DataManagerTest.java
@@ -1,8 +1,9 @@
 package cmput301w18t09.orbid;
 
 import android.test.ActivityInstrumentationTestCase2;
-
+import android.util.Log;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -12,67 +13,104 @@ import static org.junit.Assert.*;
 public class DataManagerTest extends ActivityInstrumentationTestCase2
 {
 
+    private final User user = new User("myUsername", "myemail@ualberta.ca",
+            "7805555555", "FirstName", "LastName");
+    private final Task task = new Task(user, "Task Description", "testTask90210",
+            20.00f, 1);
+
     public DataManagerTest () { super(DataManager.class); }
 
-//    public void testAddObject() {
-//        //Same as testGetObject?
-//    }
-
-    public void testGetTask() {
+    public void testTasks() {
         ArrayList<Task> taskList = new ArrayList<>();
-        DataManager.getTasks getTasksDM = new DataManager.getTasks();
-        DataManager.addTasks addTasksDM = new DataManager.addTasks();
-
-        assertTrue(taskList.isEmpty());
-
-        User user = new User("myUsername", "myemail@ualberta.ca",
-                "7805555555", "FirstName", "LastName");
-        Task task = new Task(user, "Task Description", "Task Title",
-                20.00f, 1);
-
-        addTasksDM.execute(task);
         ArrayList<String> queryList = new ArrayList<>();
-        queryList.add("_id");
-        queryList.add("AWHudg4S6isPA6QkE2B0");
-        getTasksDM.execute(queryList);
-        try {
-            taskList = getTasksDM.get();
-        } catch (Exception e) {}
-        assertFalse(taskList.isEmpty());
-    }
+        ArrayList<String> IDList = new ArrayList<>();
 
-    public void testDeleteTasks() {
-        ArrayList<Task> taskList = new ArrayList<>();
+        DataManager.addTasks addTasksDM = new DataManager.addTasks();
+        DataManager.getTasks getTasksDM = new DataManager.getTasks();
+        DataManager.getTasks getTasksDM2 = new DataManager.getTasks();
+        DataManager.getTasks getTasksDM3 = new DataManager.getTasks();
         DataManager.deleteTasks delTasksDM = new DataManager.deleteTasks();
-        DataManager.getTasks getTasksDM = new DataManager.getTasks();
-        DataManager.addTasks addTasksDM = new DataManager.addTasks();
-        ArrayList<String> queryList = new ArrayList<>();
+        queryList.add("_type");
+        queryList.add("task");
 
-        queryList.add("title");
-        queryList.add("testDeleteTasks");
+
+        //Verify task is not on server
         getTasksDM.execute(queryList);
         try {
             taskList = getTasksDM.get();
         } catch (Exception e) {}
-        assertTrue(taskList.isEmpty());
+//        Boolean testString = taskList.isEmpty();
+//        Log.i("whatsinhere", "Before Addition: Tasklist empty? " + testString.toString());
+//        assertTrue(taskList.isEmpty());
 
-        User user = new User("myUsername", "myemail@ualberta.ca",
-                "7805555555", "FirstName", "LastName");
-        Task task = new Task(user, "Task Description", "testDeleteTasks",
-                20.00f, 1);
+
+        //Add Task and verify it is on server
+        taskList.clear();
         addTasksDM.execute(task);
-
-        getTasksDM.execute(queryList);
+        getTasksDM2.execute(queryList);
         try {
-            taskList = getTasksDM.get();
+            taskList = getTasksDM2.get();
         } catch (Exception e) {}
-        assertFalse(taskList.isEmpty());
+        Integer testString = taskList.size();
+        Log.i("whatsinhere", "After Addition: Task empty? " + testString.toString());
+        for (int i = 0; i < taskList.size(); i++) {
+            Task ttask = taskList.get(i);
+            Log.i("whatsinhere", "ID: " + ttask.getID());
+        }
+//        assertFalse(taskList.isEmpty());
 
-        queryList.clear();
-        queryList.add(taskList.get(0).getID());
-        delTasksDM.execute(queryList);
+
+        //Delete task and verify it's not on server
+        if (!taskList.isEmpty()) {
+            Log.i("whatsinhere", "tasklist is " + taskList.get(0).getID());
+            IDList.add(taskList.get(0).getID());
+            delTasksDM.execute(IDList);
+        }
+        taskList.clear();
+        getTasksDM3.execute(queryList);
+        try {
+            taskList = getTasksDM3.get();
+        } catch (Exception e) {}
 //        assertTrue(taskList.isEmpty());
     }
+
+
+
+
+
+
+//    public void testDeleteTasks() {
+//        ArrayList<Task> taskList = new ArrayList<>();
+//        DataManager.deleteTasks delTasksDM = new DataManager.deleteTasks();
+//        DataManager.getTasks getTasksDM = new DataManager.getTasks();
+//        DataManager.addTasks addTasksDM = new DataManager.addTasks();
+//        ArrayList<String> queryList = new ArrayList<>();
+//
+//        queryList.add("title");
+//        queryList.add("testTask90210");
+//        getTasksDM.execute(queryList);
+//        try {
+//            taskList = getTasksDM.get();
+//        } catch (Exception e) {}
+//        assertTrue(taskList.isEmpty());
+//
+//        User user = new User("myUsername", "myemail@ualberta.ca",
+//                "7805555555", "FirstName", "LastName");
+//        Task task = new Task(user, "Task Description", "testDeleteTasks",
+//                20.00f, 1);
+//        addTasksDM.execute(task);
+//
+//        getTasksDM.execute(queryList);
+//        try {
+//            taskList = getTasksDM.get();
+//        } catch (Exception e) {}
+//        assertFalse(taskList.isEmpty());
+//
+//        queryList.clear();
+//        queryList.add(taskList.get(0).getID());
+//        delTasksDM.execute(queryList);
+//        assertTrue(taskList.isEmpty());
+//    }
 
 //    public void testUpdateObject() {
 //        ArrayList<Task> taskList = new ArrayList<>();
