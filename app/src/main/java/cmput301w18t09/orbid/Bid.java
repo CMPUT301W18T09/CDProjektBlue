5b592f1e6604dc9cacab602811507d50b2ef5655
Z-Red <araien@ualberta.ca>
2018-03-15 16:42:04 -0600

    Improve code quality

--- app/src/main/java/cmput301w18t09/orbid/Bid.java
+++ app/src/main/java/cmput301w18t09/orbid/Bid.java
@@ -1,12 +1,20 @@
 package cmput301w18t09.orbid;
 
-
+/**
+ *
+ */
 public class Bid {
 
     private User provider;
     private double price;
     private String description;
 
+    /**
+     *
+     * @param provider
+     * @param price
+     * @param description
+     */
     public Bid(User provider, double price, String description)
     {
         this.provider = provider;
@@ -14,26 +22,50 @@ public class Bid {
         this.description = description;
     }
 
+    /**
+     *
+     * @return
+     */
     public User getProvider() {
         return provider;
     }
 
+    /**
+     *
+     * @param provider
+     */
     public void setProvider(User provider) {
         this.provider = provider;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     *
+     * @param price
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     *
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
--- app/src/main/java/cmput301w18t09/orbid/User.java
+++ app/src/main/java/cmput301w18t09/orbid/User.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 import io.searchbox.annotations.JestId;
 
+/**
+ *
+ */
 public class User {
 
     private String username;
@@ -12,13 +15,19 @@ public class User {
     private String phoneNumber;
     private String firstName;
     private String lastName;
-
     @JestId
     private String ID;
-
     private ArrayList<Review> reviewList;
 
 
+    /**
+     *
+     * @param username
+     * @param email
+     * @param phoneNumber
+     * @param firstName
+     * @param lastName
+     */
     public User(String username, String email, String phoneNumber, String firstName, String lastName)
     {
         this.username = username;
@@ -29,62 +38,122 @@ public class User {
         reviewList = new ArrayList<Review>();
     }
 
+    /**
+     *
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     *
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     *
+     * @param phoneNumber
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     *
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     *
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getID() {
         return ID;
     }
 
+    /**
+     *
+     * @param ID
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Review> getReviewList() {
         return reviewList;
     }
 
+    /**
+     *
+     * @param reviewList
+     */
     public void setReviewList(ArrayList<Review> reviewList) {
         this.reviewList = reviewList;
     }
 
+    /**
+     * 
+     * @param review
+     */
     public void addReview(Review review)
     {
         this.reviewList.add(review);
