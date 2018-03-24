276acc1d1cbdf22a0c6fcce404e713a7ea098a29
AidanKosik <aidankosik@mac.com>
2018-03-24 15:57:29 -0600

    made some changes

--- app/src/main/java/cmput301w18t09/orbid/ListTaskActivity.java
+++ app/src/main/java/cmput301w18t09/orbid/ListTaskActivity.java
@@ -79,14 +79,26 @@ public class ListTaskActivity extends NavigationActivity implements ItemClickLis
         startActivity(addTask);
     }
 
+    /**
+     * Sets the refresh button visibility to false (index 0) and the add button to visible (index 1)
+     *
+     * @param menu The menu and it's items
+     * @return boolean
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.navigation, menu);
-        menu.getItem(0).setVisible(true);
+        menu.getItem(0).setVisible(false);
         menu.getItem(1).setVisible(true);
         return true;
     }
 
+    /**
+     * Sets what to do when an options button is pressed.
+     *
+     * @param item the item that was pressed
+     * @return super call to onOptionsItemSelected
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
