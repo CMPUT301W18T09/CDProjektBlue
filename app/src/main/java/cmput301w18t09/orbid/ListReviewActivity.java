b675fa4bc5f715af1e38074ead482b35272ac62d
David <davidlaycock@gmail.com>
2018-03-28 14:31:14 -0600

    Added review activity

---
+++ app/src/main/java/cmput301w18t09/orbid/ListReviewActivity.java
@@ -0,0 +1,22 @@
+package cmput301w18t09.orbid;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Created by david on 28/03/18.
+ */
+
+public class ListReviewActivity extends NavigationActivity implements ItemClickListener {
+
+    private ArrayList<Review> reviewList = new ArrayList<>();
+
+    @Override
+    protected void onCreate
+
+    @Override
+    public void onClick(View view, int position, int type) {
+
+    }
+}
