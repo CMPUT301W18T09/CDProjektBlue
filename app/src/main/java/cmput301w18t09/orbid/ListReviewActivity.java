fc868450133852e90a7afd0ea8eecfedb4ae1db4
dlaycock <dlaycock@ualberta.ca>
2018-03-28 17:26:57 -0600

    Clicking on stars now opens view review activity

--- app/src/main/java/cmput301w18t09/orbid/ListReviewActivity.java
+++ app/src/main/java/cmput301w18t09/orbid/ListReviewActivity.java
@@ -1,6 +1,10 @@
 package cmput301w18t09.orbid;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 
@@ -12,8 +16,13 @@ public class ListReviewActivity extends NavigationActivity implements ItemClickL
 
     private ArrayList<Review> reviewList = new ArrayList<>();
 
-//    @Override
-//    protected void onCreate
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        FrameLayout frameLayout = findViewById(R.id.navigation_content_frame);
+        inflater.inflate(R.layout.activity_view_reviews, frameLayout);
+    }
 
     @Override
     public void onClick(View view, int position, int type) {
